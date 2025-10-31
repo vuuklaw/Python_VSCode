0ef12fc3fc45e88de9cb4b47efb775687da4c761
vuuklaw <failedstun@gmail.com>
2025-10-31 07:35:22 -0600

    Agregar contenido a Ejercicio_1.py

---
+++ Ejercicio_1.py
@@ -0,0 +1,6 @@
+print (" *** Bienvenidos, vamos a validar como funciona una aplicacion de control de versiones ***")
+print("*** A continuación podra comprabar el resultado de diversas operaciones matematicas ***")
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
